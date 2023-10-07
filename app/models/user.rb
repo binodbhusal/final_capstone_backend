3c0483be36693b9a8d34b1080ace9157a041a3d5
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-10-07 10:47:39 +0200

    add devise api to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :api
   has_many :motors, foreign_key: :user_id, dependent: :destroy
   has_many :reservations, foreign_key: :user_id, dependent: :destroy
 

b4bf12436196ebb61667321e1540f5ea0ef0a8a8
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-11-19 17:41:10 +0000

    add user authentication test case

--- spec/requests/api/stores_location_spec.rb
+++ spec/requests/api/stores_location_spec.rb
@@ -1,6 +1,13 @@
 require 'swagger_helper'
-
+require 'devise'
 describe 'Store Locations API' do
+  let(:user) { create(:user) } # Assuming you have a factory for creating users
+
+  before do
+    # Assuming you have a sign-in method in your Devise setup
+    sign_in user
+  end
+
   path '/api/stores_location' do
     post 'Creates a store location' do
       tags 'Store Locations'
@@ -12,8 +19,10 @@ describe 'Store Locations API' do
         },
         required: ['city_name']
       }
+
       response '201', 'store location created' do
         let(:store_location) { { city_name: 'Lisbon' } }
+
         run_test! do
           expect(response).to have_http_status(:created)
         end
@@ -21,6 +30,7 @@ describe 'Store Locations API' do
 
       response '422', 'invalid request' do
         let(:store_location) { { city_name: nil } } # Invalid request to trigger a 422 response
+
         run_test! do
           expect(response).to have_http_status(:unprocessable_entity)
         end
@@ -33,6 +43,20 @@ describe 'Store Locations API' do
 
       response '200', 'list of store locations' do
         run_test!
+      end
+    end
+    post 'Unauthorized access' do
+      tags 'Store Locations'
+      produces 'application/json'
+      request_body = { city_name: 'Lisbon' }
+      before do
+        post '/api/stores_location', params: request_body.to_json, headers: { 'Content-Type': 'application/json' }
+      end
+      response '401', 'unauthorized' do
+        run_test! do
+          expect(response).to have_http_status(:unauthorized)
+          expect(response.body).to include('You need to sign in or sign up before continuing.')
+        end
       end
     end
   end

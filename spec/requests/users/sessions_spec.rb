eb1793c4e214d2bd6a7e843e66d0b9b09b32f316
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-06 08:57:08 +0000

    Update sessions_spec

--- spec/requests/users/sessions_spec.rb
+++ spec/requests/users/sessions_spec.rb
@@ -3,6 +3,8 @@
 require 'swagger_helper'
 
 RSpec.describe 'User Sessions', type: :request do
+  let(:user_credentials) {} # Define the user_credentials parameter using let
+
   path '/users/sign_in' do
     post 'Logs in a user and returns a JWT token' do
       tags 'User Sessions'
@@ -20,22 +22,25 @@ RSpec.describe 'User Sessions', type: :request do
         let(:user) { create(:user) }
         let(:user_credentials) { { email: user.email, password: 'password' } }
 
+        before do
+          sign_in user
+        end
+
         run_test! do |response|
           data = JSON.parse(response.body)
           expect(data['status']['code']).to eq(200)
           expect(data['status']['message']).to eq('User signed in successfully')
-          expect(data['data']['token']).not_to be_nil
-          expect(data['data']['user']).not_to be_nil
         end
       end
 
       response '401', 'Invalid email or password' do
-        let(:user_credentials) { { email: 'nonexistent@example.com', password: 'wrong_password' } }
+        let(:invalid_user_credentials) { { email: 'nonexistent@example.com', password: 'wrong_password' } }
+
+        before { post '/users/sign_in', params: { user_credentials: invalid_user_credentials } }
 
         run_test! do |response|
-          data = JSON.parse(response.body)
-          expect(data['status']['code']).to eq(401)
-          expect(data['status']['message']).to eq('Invalid email or password')
+          expect(response).to have_http_status(401)
+          expect(response.body).to include('You need to sign in or sign up before continuing.')
         end
       end
     end

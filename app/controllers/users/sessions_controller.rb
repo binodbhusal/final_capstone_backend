ca6fdd84b5b7f741734baec6d48e464d4a45118a
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-12-06 08:56:37 +0000

    Refactor session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,57 +1,38 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
-  before_action :authenticate_user!, except: [:create]
-
-  def create
-    user = User.find_by(email: params[:email])
-
-    if user&.valid_password?(params[:password])
-      # User's email and password are valid
-      # Generate a JWT token and return it in the response
-      token = generate_jwt_token(user)
-
-      render json: {
-        status: {
-          code: 200,
-          message: 'User signed in successfully'
-        },
-        data: {
-          token:,
-          user:
-
-        }
-      }, status: :ok
-    else
-      # Invalid email or password
-      render json: {
-        status: {
-          code: 401,
-          message: 'Invalid email or password'
-        }
-      }, status: :unauthorized
-    end
-  end
-
-  # ... other actions (e.g., respond_to_on_destroy) ...
 
   private
 
-  def generate_jwt_token(user)
-    # Your JWT token generation logic here
-    # Typically, you would use a gem like 'jwt' to generate tokens
-    # Example:
-    payload = { sub: user.id, email: user.email }
-    secret_key = Rails.application.credentials.fetch(:secret_key_base)
-    JWT.encode(payload, secret_key, 'HS256')
+  def respond_with(_resource, _options = {})
+    render json: {
+             status: {
+               code: 200,
+               message: 'User signed in successfully',
+               data: current_user
+             }
+           },
+           status: :ok
   end
 
   def respond_to_on_destroy
-    sign_out
-    render json: {
-      status: {
-        code: 200,
-        message: 'Logout successfully'
-      }
-    }, status: ok
+    jwt_payload = JWT.decode(request.headers['Authorization'].chars[1],
+                             Rails.application.credentials.fetch(:secret_key_base)).first
+
+    current_user = User.find(jwt_payload['sub'])
+
+    if current_user
+      sign_out(current_user)
+      render json: {
+               status: 200,
+               message: 'Signed out succesfully'
+             },
+             status: :ok
+    else
+      render json: {
+               status: 401,
+               message: 'User has no active session'
+             },
+             status: :unauthorized
+    end
   end
 end
